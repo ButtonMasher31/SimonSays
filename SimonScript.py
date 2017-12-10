df458d316d7dc50d610108190323b55f77ea79f7
ButtonMasher31 <tcmortimer@outlook.com>
2017-12-10 18:11:17 +0000

    Sequence Generator

--- SimonScript.py
+++ SimonScript.py
@@ -8,16 +8,24 @@ led_blue = LED(16)
 led_green = LED(20)
 led_red = LED(12)
 led_yellow = LED(7)
-led_rgb = RGBLED(red=14, green=15, blue=18)
+led_rgb = RGBLED(red=18, green=15, blue=14)
 button_blue = Button(23)
 button_green = Button(24)
 button_red = Button(25)
 button_yellow = Button(8)
 buzzer = Buzzer(21)
 
-#Create lists
+#Create lists and variables
 correct_sequence = []
 input_sequence = []
+availible_colours = ["blue", "green", "red", "yellow"]
+colours = []
+max_sequence_length = random.randint(1, 10)
+sequence_length = 0
+blue = (0, 0, 1)
+red = (1, 0, 0)
+green = (0, 1, 0)
+yellow = (0.25, 0.3, 0.25)
 
 #Button detection function
 def button_detect():
@@ -33,3 +41,22 @@ def button_detect():
     elif button_yellow.is_pressed:
         input_sequence.append("yellow")
         sleep(0.5)
+
+#Sequence generator function
+def sequence_generator():
+    sequence_length =+ 1
+    for i in range(0, sequence_length):
+        correct_sequence.append(availible_colours[random.randint(0, 3)])
+    for i in range(0, sequence_length):
+        if correct_sequence[i] == "blue":
+            colours.append(blue)
+        elif correct_sequence[i] == "red":
+            colours.append(red)
+        elif correct_sequence[i] == "green":
+            colours.append(green)
+        elif correct_sequence[i] == "yellow":
+            colours.append(yellow)
+        print(correct_sequence)
+sequence_generator()
+sleep(5)
+sequence_generator()
